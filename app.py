3a4c2529b9503cb17aab1aedb6237b3b14bbf0b6
gagan1j <gaganj.is22@bmsce.ac.in>
2025-02-06 08:53:32 +0000

    app.py updated

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/proud-bear-11")
 def home():
-    return "Hello, Jenkins Multi-Stage Pipeline!"
+    return "health: All systems operational"
 
 if __name__ == "__main__":
     app.run(debug=True)
